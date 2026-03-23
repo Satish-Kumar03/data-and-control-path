f0e3a7b1ae33a353585e978c2cc72040c6a0ca38
Satish Kumar <satishkumar250303@gmail.com>
2026-03-23 18:51:52 +0530

    Refactor GCDdata, PIPO, and MUX modules
    Updated module ports and signal names for clarity.

--- gcd_data.v
+++ gcd_data.v
@@ -1,5 +1,5 @@
 module GCDdata (
-    gt, lt, eq, ldA, ldB, sel_in, clk, data_in, sel1, sel2
+    gt, lt, eq, ldA, ldB, sel_in, clk, data_in, sel1, sel2      // data path gives output gt, lt and eq to controller.
 );
     output gt, lt, eq;
     input ldA, ldB, sel_in, clk, sel1, sel2;
@@ -16,18 +16,18 @@ module GCDdata (
 
 endmodule
 
-module PIPO(data_out, data_in, ldA,clk);
+module PIPO(data_out, data_in, load, clk);
    input [15:0]data_in;
-   input clk, ldA;
+   input clk, load;
    output reg [15:0] data_out;
 
    always @(posedge clk)
       begin 
-        if(ldA) data_out <= data_in;
+          if(load) data_out <= data_in;
       end
 endmodule
 
-module MUX(out, sel, in0, in1);
+module MUX(out, sel, in0, in1);     // always use mux in0 -> 0 and in1 -> 1
     input [15:0] in0, in1;
     input sel;
     output [15:0] out;
@@ -46,7 +46,7 @@ endmodule
 
 module SUB(subout, in0, in1);
     input [15:0] in0, in1;
-    output reg [15:0] subout;
+    output reg [15:0] subout;  // do not use assign statement, as it requires to store the result. hence used reg. 
 
     always @(*) begin
         subout = in0-in1;
