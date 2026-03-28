82ad1b3ae2c527756be94bdd6dca402461ae01db
Satish Kumar <satishkumar250303@gmail.com>
2026-03-28 21:01:50 +0530

    Add inputs and outputs to multidata module

--- multi_data.v
+++ multi_data.v
@@ -1,3 +1,14 @@
-module multidata();
+module multidata(qm1,clk,ldA,ldQ,ldM,clrA,clrQ,clrff,shftA,shftQ,addsub, data_in);
+  output qm1;
+  input clk, ldA, ldQ, ldM, clrA, clrQ, clrff, shftA, shftQ, addsub;
+  input [7:0] data_in;
+  wire [7:0] bus, Qout, Mout, Aout, z;
+  reg [7:0] A=0;
+
+  PIPO Q(Qout,bus,ldQ,clk);
+  PIPO M(Mout,bus,ldM,clk);
+  PIPO A(Aout,z,ldA,clk);
+  MUX M1(z,Aout,Mout,clk);
+  SHIFT S(
   
 endmodule
