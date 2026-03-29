5f5b79d06a6b1d4e6aa5965ce0adb0347a161703
Satish Kumar <satishkumar250303@gmail.com>
2026-03-29 11:58:28 +0530

    Refactor signal names and remove unused register

--- multi_data.v
+++ multi_data.v
@@ -2,13 +2,13 @@ module multidata(qm1,clk,ldA,ldQ,ldM,clrA,clrQ,clrff,shftA,shftQ,addsub, data_in
   output qm1;
   input clk, ldA, ldQ, ldM, clrA, clrQ, clrff, shftA, shftQ, addsub;
   input [7:0] data_in;
-  wire [7:0] bus, Qout, Mout, Aout, z;
-  reg [7:0] A=0;
+  wire [7:0] bus, Q, M, A, z;
+  //reg [7:0] A=0;
 
   PIPO Q(Qout,bus,ldQ,clk);
   PIPO M(Mout,bus,ldM,clk);
   PIPO A(Aout,z,ldA,clk);
   MUX M1(z,Aout,Mout,clk);
-  SHIFT S(
+  SHIFT S(A,Q,
   
 endmodule
