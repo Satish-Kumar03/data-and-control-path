857f68ed153448699c3a0fee3485441910f55a15
Satish Kumar <satishkumar250303@gmail.com>
2026-04-29 22:57:29 +0530

    Modify ricsv module to include clock inputs
    Added clock inputs and parameters for instruction set.

--- RISC_V.v
+++ RISC_V.v
@@ -1,3 +1,10 @@
-module ricsv();
+module ricsv(clk1,clk2);
+  parameter data_size = 32, mem_size = 1024;
+  parameter ADD=000000, SUB=000001, AND=000010, OR=000011, SLT=000100, MUL=000101, HLT=111111,
+            LW=001000, SW=001001,
+  
+  input clk1, clk2;
+  reg [data_size-1:0] reg_bank[data_size-1:0];
+  reg [data_size-1:0] mem[mem_size-1:0];
   
 endmodule 
