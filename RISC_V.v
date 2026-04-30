c6e00ab82018b1cd4ad39ddffa2d50446538d620
Satish Kumar <satishkumar250303@gmail.com>
2026-04-30 21:40:13 +0530

    Remove addr_size parameter from RISC_V module

--- RISC_V.v
+++ RISC_V.v
@@ -1,5 +1,5 @@
 module ricsv(clk1,clk2);
-  parameter data_size = 32, mem_size = 1024, addr_size=5;
+  parameter data_size = 32, mem_size = 1024;
   parameter ADD=000000, SUB=000001, AND=000010, OR=000011, SLT=000100, MUL=000101, HLT=111111,
             LW=001000, SW=001001, ADDI=001010, SUBI=001011, SLTI=001100, BNEQZ=001101, BEQZ=001110;
   
