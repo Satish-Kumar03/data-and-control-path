fb44d87bde9c9402b01e495e13a746d240405664
Satish Kumar <satishkumar250303@gmail.com>
2026-05-12 19:47:26 +0530

    Rename module from ricsv to rics_v

--- RISC_V.v
+++ RISC_V.v
@@ -1,9 +1,9 @@
-module ricsv(clk1,clk2);
+module rics_v(clk1,clk2);
   parameter data_size = 32, mem_size = 1024;
   parameter ADD=6'b000000, SUB=6'b000001, AND=6'b000010, OR=6'b000011, SLT=6'b000100, MUL=6'b000101, XOR=6'b000110, SLL= 6'b000111, 
-            SRL=6'b001000, HLT=6'b111111,   // R-R type 
+            SRL=6'b001000, HLT=6'b111111,                                                                      // R-R type 
             LW=6'b001000, SW=6'b001001, ADDI=6'b001010, SUBI=6'b001011, SLTI=6'b001100, BNEQZ=6'b001101, BEQZ=6'b001110, XORI=6'b001111
-            SLLI=6'b010000, SRLI=6'b010001;  // R-I type
+            SLLI=6'b010000, SRLI=6'b010001;                                                                   // R-I type
   parameter RR_ALU = 3'b000, RM_ALU=3'b001, LOAD=3'b010, STORE=3'b011, BRANCH=3'b100, HALT=3'b101;
   
   input clk1, clk2;
@@ -12,15 +12,15 @@ module ricsv(clk1,clk2);
   reg [data_size-1:0] mem[0:mem_size-1];
   reg [2:0] ID_EX_type, EX_MEM_type, MEM_WB_type;
   
-  reg [data_size-1:0] IF_ID_IR, IF_ID_NPC, PC;             // for instruction fetch and instruction decode stage
+  reg [data_size-1:0] IF_ID_IR, IF_ID_NPC, PC;                               // for instruction fetch and instruction decode stage
   
-  reg [data_size-1:0] ID_EX_A,ID_EX_B,ID_EX_IR, ID_EX_IMM;       // for exceution stage
+  reg [data_size-1:0] ID_EX_A,ID_EX_B,ID_EX_IR, ID_EX_IMM;                  // for exceution stage
   // reg ID_EX_NPC;
   
-  reg [data_size-1:0] EX_MEM_ALUout, EX_MEM_B, EX_MEM_IR, EX_MEM_NPC;            //  for memory stage
+  reg [data_size-1:0] EX_MEM_ALUout, EX_MEM_B, EX_MEM_IR, EX_MEM_NPC;                   //  for memory stage
   reg EX_MEM_COND;
   
-  reg [data_size-1:0] MEM_WB_LMD, MEM_WB_IR, MEM_WB_B, MEM_WB_ALUout;     // forwrite back stage 
+  reg [data_size-1:0] MEM_WB_LMD, MEM_WB_IR, MEM_WB_B, MEM_WB_ALUout;                   // forwrite back stage 
 
   always @(posedge clk1)                                                                    // IF stage 
     if(HALTED == 0)
