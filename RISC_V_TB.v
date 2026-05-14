218c779266464c4504d61c2b91115c622c537869
Satish Kumar <satishkumar250303@gmail.com>
2026-05-14 19:00:16 +0530

    Initialize memory with RISC-V instructions
    Added instructions to the RISC-V testbench for testing basic operations and displaying register values.

--- RISC_V_TB.v
+++ RISC_V_TB.v
@@ -20,6 +20,23 @@ module riscv_tb;
 
   initial
     begin
-      riscv.mem[0] = 32'h
-    
+      riscv.mem[0] = 32'h2801000A;     // ADDI R1 R0 10;
+      riscv.mem[1] = 32'h28020014;     // ADDI R2 R0 20;
+      riscv.mem[2] = 32'h28030019;     // ADDI R3 R0 25;
+      riscv.mem[3] = 32'h0ce77800;     // OR R7 R7 R7;
+      riscv.mem[4] = 32'h0ce77800;     // OR R7 R7 R7;
+      riscv.mem[5] = 32'h00412000;     // ADD R4 R1 R2;
+      riscv.mem[6] = 32'h0ce77800;     // OR R7 R7 R7;
+      riscv.mem[7] = 32'h00685000;     // ADD R5 R4 R3;
+      riscv.mem[8] = 32'hfc000000;     // halt
+
+      riscv.HALTED = 0;
+      riscv.BR_TAKEN =0;
+      riscv.PC =0;
+
+      #300
+      for(i=0; i<6; i=i+1)
+        $display("R%1d = %2d", i, riscv.reg_bank[i])
+    end
+  
 endmodule 
